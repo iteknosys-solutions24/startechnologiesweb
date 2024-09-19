eac24b3f634032d228ef69263e7db34d9dcec7da
iteknosys-solutions24 <iteknosys.solutions24@gmail.com>
2024-09-19 14:11:58 +0530

    Update views.py
    Contact backend

--- starweb_app/views.py
+++ starweb_app/views.py
@@ -16,26 +16,41 @@ def index_view(request):
         subject = request.POST.get('csubject')
         message = request.POST.get('cmessage')
 
+        subject_to_user = 'Star Technologies - Thank You for Your Query!'
+        message_to_user = f"""
+    <p>Dear {name},</p>
+    <p>Thank you for reaching out to us. We appreciate your interest in our company and are happy to help answer any questions you may have. We will respond to your inquiry as soon as possible.</p>
+    <p>You can check out the below link to view our Company Profile:</p>
+    
+        <b><i><a href="https://tinyurl.com/5cr8229h" target="_blank" style="font-size: large;">Our Company Profile</a></i></b>
+    
+    
+    <p><b>Best Regards</b>,<br><i>Star Technologies</i></p>
+    """
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [email]  # Replace with actual recipient email
+
+            # Send thank you email to user
+        send_mail(
+                 subject_to_user,
+                message_to_user,
+                from_email,
+                recipient_list,
+                fail_silently=False,
+                html_message=message_to_user  # Use HTML message
+            )
+        
         send_mail(
                 'Client Enquiry Form: ',
-                f'From: {name}\n Email: {email}\n\n Message:\n{message}',
+                f'From: {name}\nEmail: {email}\n\nMessage:\n{message}',
             settings.EMAIL_HOST_USER,  # Replace with owner's email
             [settings.EMAIL_HOST_USER],  # Replace with owner’s email
             fail_silently=False,
              
             )
 
-            # Send thank you email to user
-        send_mail(
-                'Star Technologies',
-                'Thank you for your message. We will get back to you shortly.',
-                settings.EMAIL_HOST_USER,
-                [email],
-                fail_silently=False,
-            )
-
         messages.success(request, "Thank you for your message! We will get back to you soon.")
-        print('no sent')
+    
         return redirect('index-view')
 
     return render(request, 'html/index_view.html')
