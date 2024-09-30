73079a742588ef70de2d9f0249f89531718b2b46
iteknosys-solutions24 <iteknosys.solutions24@gmail.com>
2024-09-30 13:26:33 +0530

    Update urls.py

--- starweb_app/urls.py
+++ starweb_app/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import index_view,products_view,accept_cookies,reject_cookies,cookie_policy,BP_20M_31_28_24_22_pdf_view,BP_20C_20_25_10C20_pdf_view,BP_30M_35_31_28_pdf_view,PC_311W_MC_320F_merged,MC_251FW,BP_30C25Z_BP_30C25ZT,BP_50C26_50C31_50C36_50C45,M_8124cidn,PN_Q901_801_701_6011,PN_HW_751,PN_50TC1,Display_R65_2024_2,R_75_inch,display_r_series_86_inch,viewsonic_all_pdf,epson_all_pdf,panasonic_all_pdf,infocus_all_pdf,benq_all_pdf,optoma_all_pdf,fujitsu_all_pdf,hp_all_pdf
+from .views import index_view,products_view,BP_20M_31_28_24_22_pdf_view,BP_20C_20_25_10C20_pdf_view,BP_30M_35_31_28_pdf_view,PC_311W_MC_320F_merged,MC_251FW,BP_30C25Z_BP_30C25ZT,BP_50C26_50C31_50C36_50C45,M_8124cidn,PN_Q901_801_701_6011,PN_HW_751,PN_50TC1,Display_R65_2024_2,R_75_inch,display_r_series_86_inch,viewsonic_all_pdf,epson_all_pdf,panasonic_all_pdf,infocus_all_pdf,benq_all_pdf,optoma_all_pdf,fujitsu_all_pdf,hp_all_pdf,cookies_policy_view
 
 
 urlpatterns = [
     path('',index_view,name='index-view'),
     path('products/', products_view, name='products-view'),
-    path('accept-cookies/', accept_cookies, name='accept_cookies'),
-    path('reject-cookies/', reject_cookies, name='reject_cookies'),
-    path('cookie-policy/', cookie_policy, name='cookie-policy'),
+    path('cookies-policy/', cookies_policy_view, name='cookies-policy'),
+ 
     
     #Products
     path('BP-20M-31-28-24-22-pdf/', BP_20M_31_28_24_22_pdf_view, name='BP-20M-31-28-24-22-pdf-view'),
@@ -37,6 +36,5 @@ urlpatterns = [
 
     #other products
     path('benq-all-pdf/', fujitsu_all_pdf, name='benq-all-pdf'),
-    path('hp-all-pdf/', hp_all_pdf, name='hp-all-pdf'),
-    
+    path('hp-all-pdf/', hp_all_pdf, name='hp-all-pdf'),  
 ]
