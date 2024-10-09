02d49ee7429e1841374273c3553e7b02cd4dd31d
iteknosys-solutions24 <iteknosys.solutions24@gmail.com>
2024-10-09 13:30:14 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -30,3 +30,54 @@ document.addEventListener("DOMContentLoaded", function () {
     hideBanner(); // Hide banner after declining
   });
 });
+
+//modal view
+
+let currentIndex = 0;
+const images = document.querySelectorAll(".gallery-image");
+
+function openModal(image) {
+  currentIndex = Array.from(images).indexOf(image);
+  showModal(image.src, image.alt);
+}
+
+function showModal(src, alt) {
+  const modal = document.getElementById("imageModal");
+  const modalImage = document.getElementById("modalImage");
+  const caption = document.getElementById("caption");
+
+  modal.style.display = "flex"; // Use flex to center the modal
+  modalImage.src = src;
+  caption.innerHTML = alt;
+}
+
+function closeModal() {
+  const modal = document.getElementById("imageModal");
+  modal.style.display = "none";
+}
+
+function changeImage(direction) {
+  currentIndex = (currentIndex + direction + images.length) % images.length; // Loop back to start or end
+  showModal(images[currentIndex].src, images[currentIndex].alt);
+}
+
+// Close the modal when clicking on the close button
+document.querySelector(".close").onclick = closeModal;
+
+// Close the modal when clicking outside of the image
+document.getElementById("imageModal").onclick = function (event) {
+  if (event.target === this) {
+    closeModal();
+  }
+};
+
+//Scroll Top
+
+window.onscroll = function () {
+  const button = document.getElementById("topButton");
+  if (document.body.scrollHeight - window.scrollY <= window.innerHeight + 100) {
+    button.style.display = "block"; // Show button when near bottom
+  } else {
+    button.style.display = "none"; // Hide button otherwise
+  }
+};
